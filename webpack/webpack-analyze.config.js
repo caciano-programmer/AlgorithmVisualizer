b5deb1fda7a56168e06ee70e65ec54fb2b904e81
caciano almeida <oalme004@fiu.edu>
2021-02-23 15:25:33 -0500

    testing gh pages

--- webpack/webpack-analyze.config.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = {
-  plugins: [new BundleAnalyzerPlugin()],
-};
